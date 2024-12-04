8faed2d54ab804cae99b7d800ed57f9dad761b78
tmid3 <106893423+tmid1@users.noreply.github.com>
2024-12-04 10:44:57 -0700

    change db init

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,7 +1,7 @@
-CREATE TABLE users(
-    username VARCHAR(50) PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    password CHAR(60) NOT NULL,
-    reset_token VARCHAR(255),
-    reset_token_expires TIMESTAMP
+CREATE TABLE IF NOT EXISTS users(
+        username VARCHAR(50) PRIMARY KEY,
+        email VARCHAR(50) NOT NULL,
+        password CHAR(60) NOT NULL,
+        reset_token VARCHAR(255),
+        reset_token_expires TIMESTAMP
 );
